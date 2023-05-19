ba0d449ba3fb22d7b499ae3db1a1afbe839d414b
Nathan Krasney <natankrasney@gmail.com>
2023-05-19 09:20:00 +0300

    remove un used code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
-import { Color } from "../types/e-color";
 import { IEnumKeyValue } from "../types/i-enum-key-value";
 
 
-export function getColorEnumKeyValues(): IEnumKeyValue[] {
-  return getEnumKeyValues(Color);
-}
 
 export function getEnumKeyValues(_enum: Object): IEnumKeyValue[] {
   const enumKeyValues: IEnumKeyValue[] = [];
